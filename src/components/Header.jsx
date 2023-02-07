025289e0caec348b06205993a6d40383c5ddcf11
Rishabh Singh <singhrishabh1670@gmail.com>
2023-02-07 23:42:51 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,18 @@ import React from "react"
 import Logo from "./img/logo.png"   //logo initialized
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md"; //cart logo initilized
 import Avatar from "./img/avatar.png"
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Header = () => {
     return(
         <header className="fixed z-50 w-screen  p-6 px-16">
             {/* For Desktop and tablet for medium and large device it should be flex for small device hidden*/}
             <div className="hidden md:flex w-full h-full items-center justify-between">
-                <div className="flex items-center gap-2">
+                <Link to={"/"} className="flex items-center gap-2">
                     <img src={Logo} className="w-8 object-cover" alt="logo"></img> {/*logo size w-8 */}
                     <p className="text-headingColor text-xl font-bold"> City</p> {/*logo text*/}
-                </div>
+                </Link>
                 <div className="flex items-center gap-8">
                 <ul className="flex items-center gap-8">
                     <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
@@ -26,7 +28,11 @@ const Header = () => {
                         <p className="text-sm text-white font-semibold">2</p> 
                     </div>
                 </div>
-                <img src={Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl" alt="userprofile"/>
+                <motion.img 
+                whileTap={{scale: 0.6}}
+                src={Avatar} 
+                className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" 
+                alt="userprofile"/>
                 </div>
             </div>
             {/* for mobile for small device flexible and for medium device it should be hidden*/}
